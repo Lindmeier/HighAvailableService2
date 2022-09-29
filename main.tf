75853a7b6dea4fd34b8f5910c2244e1d489cf090
xgadlnd <Jan.Lindmeier@atruvia.de>
2022-09-29 07:25:15 +0200

    test

--- main.tf
+++ main.tf
@@ -3,13 +3,18 @@ variable "provider_token"{
     type = string
 }
 provider "fakewebservices" {
+  alias  = "3e53191be96d4e8bba6689bdafbb9dee"
   token = var.provider_token
 }
 
 # Module Calls (Instanziierung der Bausteine)
-module "HA_SERVICE_3e53191b-e96d-4e8b-ba66-89bdafbb9dee" {
+module "HA_SERVICE_3e53191be96d4e8bba6689bdafbb9dee" {
   source  = "app.terraform.io/team-automation/higlyAvailableService/tfdp"
   version = "1.0.0"
+  providers = {
+    fakewebservices = fakewebservices.3e53191be96d4e8bba6689bdafbb9dee
+
+  }
   vm_type = "webserver"
 }
 
