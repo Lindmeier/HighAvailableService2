b6584c554984df83098491109f018af33cef61a7
xgadlnd <Jan.Lindmeier@atruvia.de>
2022-09-29 10:39:10 +0200

    add product

--- main.tf
+++ main.tf
@@ -18,7 +18,21 @@ module "HA_SERVICE_3e53191be96d4e8bba6689bdafbb9dee" {
   vm_type = "webserver"
 }
 
+module "HA_SERVICE_3e53191be91651a6689bdafbb9dee" {
+  source  = "app.terraform.io/team-automation/higlyAvailableService/tfdp"
+  version = "1.0.0"
+  providers = {
+    fakewebservices = fakewebservices.3e53191be96d4e8bba6689bdafbb9dee
+
+  }
+  vm_type = "restservice"
+}
+
 # Output
-output "LB_ID" {
-  value = module.HA_SERVICE_3e53191b-e96d-4e8b-ba66-89bdafbb9dee.lb_id
+output "LB_ID_3e53191be96d4e8bba6689bdafbb9dee" {
+  value = module.HA_SERVICE_3e53191be96d4e8bba6689bdafbb9dee.lb_id
+}
+
+output "LB_ID_3e53191be91651a6689bdafbb9dee" {
+  value = module.HA_SERVICE_3e53191be91651a6689bdafbb9dee.lb_id
 }
