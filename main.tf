c56a0eb005a6d8a445e2bad4e9025864a1d5252d
xgadlnd <Jan.Lindmeier@atruvia.de>
2022-09-30 08:31:40 +0200

    Produkt entfernt

--- main.tf
+++ main.tf
@@ -14,18 +14,18 @@ module "HA_SERVICE_3e53191be96d4e8bba6689bdafbb9dee" {
   vm_type = "webserver"
 }
 
-module "HA_SERVICE_3e53191be91651a6689bdafbb9dee" {
-  source  = "app.terraform.io/team-automation/higlyAvailableService/tfdp"
-  version = "1.0.0"
+# module "HA_SERVICE_3e53191be91651a6689bdafbb9dee" {
+#   source  = "app.terraform.io/team-automation/higlyAvailableService/tfdp"
+#   version = "1.0.0"
   
-  vm_type = "restservice"
-}
+#   vm_type = "restservice"
+# }
 
 # Output
 output "LB_ID_3e53191be96d4e8bba6689bdafbb9dee" {
   value = module.HA_SERVICE_3e53191be96d4e8bba6689bdafbb9dee.lb_id
 }
 
-output "LB_ID_3e53191be91651a6689bdafbb9dee" {
-  value = module.HA_SERVICE_3e53191be91651a6689bdafbb9dee.lb_id
-}
+# output "LB_ID_3e53191be91651a6689bdafbb9dee" {
+#   value = module.HA_SERVICE_3e53191be91651a6689bdafbb9dee.lb_id
+# }
